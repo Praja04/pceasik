b4572240d523fb0779c1a5e1afbed40c490ebac1
Damang Maulana Wirapraja <87636009+Praja04@users.noreply.github.com>
2024-03-27 09:02:55 +0700

    Update Database.php

--- project-name/app/Config/Database.php
+++ project-name/app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'project-cbi',
+        'database'     => 'project',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
